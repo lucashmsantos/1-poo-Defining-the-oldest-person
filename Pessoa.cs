2b173114eacc6313aa5963796734348df54b5fc3
Lucas SANTOS <lucas.santos-1@alstomgroup.com>
2024-01-26 14:54:58 -0300

    Add project files.

---
+++ Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace pooMaiorIdade
+{
+    public class Pessoa  /*Para poder usar em qualquer lugar do programa ou outro projeto*/
+    {
+        //Construtores
+        public Pessoa()
+        {
+            this.Nome = " ";
+            this.Idade = 0;
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        //Propriedades
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }  /*Guardar o valor em Maiusculo*/
+        }
+
+        private int idade;   /*Garantir que nao vai existir valor negativo*/
+        public int Idade {
+            get { return idade; }
+            set {
+            if (value >= 0)
+                {
+                    idade = value;
+                }
+                else
+                {
+                    this.idade = 0;
+                }
+            }
+        
+        }   /*Pega o Valor e armazena o valor*/
+
+        //Metodos
+        public void ExibirDados()  /*Nao retorna valor algum*/
+        {
+            Console.WriteLine("Nome:  " + this.Nome);
+            Console.WriteLine("Idade: " + this.Idade);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace pooMaiorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1;
+            Pessoa p2;
+            Pessoa p3;
+
+            Console.WriteLine("Determina qual pessoa é a mais velha.");
+
+            //primeirapessoa
+            Console.Write("Nome da 1 pessoa:  ");
+            p1 = new Pessoa();
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade da 1 pessoa:  ");
+            p1.Idade = Convert.ToInt32(Console.ReadLine());
+
+            //segundapessoa
+            Console.Write("Nome da 2 pessoa:  ");
+            p2 = new Pessoa();
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade da 2 pessoa:  ");
+            p2.Idade = Convert.ToInt32(Console.ReadLine());
+
+            //segundapessoa
+            Console.WriteLine("Nome da 3 pessoa: ");
+            string nome = Console.ReadLine();
+            Console.Write("Idade da 3 pessoa: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            p3 = new Pessoa(nome, idade);
+
+
+            //Logica
+
+            if ((p1.Idade > p2.Idade) && (p1.Idade > p3.Idade))
+            {
+                Console.WriteLine("A pessoa mais velha é:  ");
+                p1.ExibirDados();
+            }
+            else
+            {
+                if ((p2.Idade > p3.Idade) && (p2.Idade > p1.Idade))
+                {
+                    Console.WriteLine("A pessoa mais velha é:  ");
+                    p2.ExibirDados();
+                }
+                else
+                {
+                    Console.WriteLine("A pessoa mais velha é:  ");
+                    p3.ExibirDados();
+                }
+            }
+        }
+    }
+}
